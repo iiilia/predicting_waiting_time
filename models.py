257df7b3e71c90c350e9283c1d74febba1b33575
Ilya Ivanitskiy <iiivanitskiy@avito.ru>
2017-05-02 11:10:24 +0300

    add multithreading + minor bug fix

--- models.py
+++ models.py
@@ -24,19 +24,23 @@ import xgboost as xgb
 
 class AddFeatures(BaseEstimator, RegressorMixin):
     
-    def __init__(self, model, stop=False):
+    def __init__(self, model, score, stop=False, verbose = False):
+        self.verbose = verbose
         self.model = model
         self.models = []
         self.features = []
-        self.score = [sys.float_info.max]
+        self.scores = [sys.float_info.max]
         self.final_model = None
         self.stop = stop
         self.best_iter = None
+        self.score = score
+        self.name = self.model.__str__().split('(')[0]
+        self.main_name = self.__str__().split('(')[0]
     
-    def fit(self, df, df_val, features, target, score):
+    def fit(self, df, df_val, features, target):
         # score: less -> better
         
-        logging.info('*** '+self.model.__str__())
+        logging.info('*** '+self.name)
         
         self.all_features = features
         self.flag_stop = True
@@ -46,7 +50,7 @@ class AddFeatures(BaseEstimator, RegressorMixin):
         while self.flag_stop and self.all_features:
             
             counter = counter + 1
-            if counter % 10 == 0:
+            if counter % 10 == 0 and self.verbose:
                 sys.stderr.write('-')
                 
             res = []
@@ -55,17 +59,17 @@ class AddFeatures(BaseEstimator, RegressorMixin):
                 model.fit(df[self.features + [feat]],df[target])
                 y_pred = model.predict(df_val[self.features + [feat]])
                 y_real = df_val[target]
-                temp_score = score(y_real, y_pred)
+                temp_score = self.score(y_real, y_pred)
                 res.append((feat, temp_score, model))
             
             res_sorted = sorted(res, key=lambda x: x[1])  
             
-            logging.info('{} - {}'.format(res_sorted[0][0],res_sorted[0][1]))
+            logging.info('{}/{} : {} - {}'.format(self.main_name, self.name, res_sorted[0][0],res_sorted[0][1]))
             
             if res_sorted[0][1] < self.score[-1]:
                 self.all_features.remove(res_sorted[0][0])
                 self.features.append(res_sorted[0][0])
-                self.score.append(res_sorted[0][1])
+                self.scores.append(res_sorted[0][1])
                 self.models.append(res_sorted[0][2])
             else:
                 if self.stop:
@@ -73,12 +77,12 @@ class AddFeatures(BaseEstimator, RegressorMixin):
                 else:
                     self.all_features.remove(res_sorted[0][0])
                     self.features.append(res_sorted[0][0])
-                    self.score.append(res_sorted[0][1])
+                    self.scores.append(res_sorted[0][1])
                     self.models.append(res_sorted[0][2])
         
-        self.final_model, self.final_score, self.best_iter = sorted(zip(self.score, 
+        self.final_model, self.final_score, self.best_iter = sorted(zip(self.scores, 
                                                         self.models,
-                                                        range(1,len(self.score)+1)), key=lambda x: x[0])[0]
+                                                        range(1,len(self.scores)+1)), key=lambda x: x[0])[0]
         self.final_features = self.features[:self.best_iter]
         
         
@@ -88,19 +92,23 @@ class AddFeatures(BaseEstimator, RegressorMixin):
     
 class ExceptFeatures(BaseEstimator, RegressorMixin):
     
-    def __init__(self, model, stop=False):
+    def __init__(self, model, score, stop=False, verbose = False):
+        self.verbose = verbose
         self.model = model
         self.models = []
         self.excepted_features = [None]
-        self.score = []
+        self.scores = []
         self.final_model = None
         self.final_score = None
         self.final_iter = None
         self.stop = stop
-    
-    def fit(self, df, df_val, features, target, score):
+        self.score = score
+        self.name = self.model.__str__().split('(')[0]
+        self.main_name = self.__str__().split('(')[0]
+        
+    def fit(self, df, df_val, features, target):
         # score: less -> better
-        logging.info('*** '+self.model.__str__())
+        logging.info('*** '+self.name )
         
         self.features = features
         self.flag_stop = True
@@ -108,14 +116,14 @@ class ExceptFeatures(BaseEstimator, RegressorMixin):
         model.fit(df[self.features],df[target])
         y_pred = model.predict(df_val[self.features])
         y_real = df_val[target]
-        temp_score = score(y_real, y_pred)
-        self.score.append(temp_score)
+        temp_score = self.score(y_real, y_pred)
+        self.scores.append(temp_score)
         self.models.append(model)
         counter = 0
         while self.flag_stop and len(set(self.features).difference(set(self.excepted_features)))>0:
             
             counter = counter + 1
-            if counter % 10 == 0:
+            if counter % 10 == 0 and self.verbose:
                 sys.stderr.write('-')
             res = []
             
@@ -126,29 +134,28 @@ class ExceptFeatures(BaseEstimator, RegressorMixin):
                 model.fit(df[temp_features],df[target])
                 y_pred = model.predict(df_val[temp_features])
                 y_real = df_val[target]
-                temp_score = score(y_real, y_pred)
+                temp_score = self.score(y_real, y_pred)
                 res.append((feat, temp_score, model ))
             
-            # sys.stderr.write('|||'.join(self.excepted_features))
             res_sorted = sorted(res, key=lambda x: x[1])  
             
-            logging.info('{} - {}'.format(res_sorted[0][0],res_sorted[0][1]))
+            logging.info('{}/{} : {} - {}'.format(self.main_name, self.name, res_sorted[0][0],res_sorted[0][1]))
             
             if res_sorted[0][1] < self.score[-1]:
                 self.excepted_features.append(res_sorted[0][0])
-                self.score.append(res_sorted[0][1])
+                self.scores.append(res_sorted[0][1])
                 self.models.append(res_sorted[0][2])
             else:
                 if self.stop:
                     self.flag_stop = False
                 else:
                     self.excepted_features.append(res_sorted[0][0])
-                    self.score.append(res_sorted[0][1])
+                    self.scores.append(res_sorted[0][1])
                     self.models.append(res_sorted[0][2])
                     
-        self.final_model, self.final_score, self.final_iter = sorted(zip(self.score, 
+        self.final_model, self.final_score, self.final_iter = sorted(zip(self.scores, 
                                                         self.models,
-                                                       range(len(self.score))), key=lambda x: x[0])[0]
+                                                       range(len(self.scores))), key=lambda x: x[0])[0]
         
         temp_features = self.features
         for i in self.excepted_features[:self.best_iter]:
